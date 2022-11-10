d370a03e20c792cd61be2cc624e1be35d9a869dc
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-11-10 15:13:43 +0000

    Reformatted, and added family doctor and lab orders

--- README.md
+++ README.md
@@ -9,6 +9,19 @@ Very much WIP. Proper docs and tests to come.
 ### Todo
 
 - [ ] Finish basic conversion mapping
+- [ ] Fix data conversions (e.g. datetimes and enums)
 - [ ] Add convenience functions to convert an XML file (as opposed to an already-parsed XSData object)
 - [ ] Add tests
 - [ ] Add docs
+
+## Developer notes
+
+### Line length
+
+For this repository (and _only_ this repository), we allow extra long lines.
+This is because the schema maps are more readable if we have one element per line.
+If we find in the future that this is causing issues for developers, we will consider re-introducing the line length limit.
+
+For now, we effectively disable the line length limit by setting it to a large number:
+
+`black --line-length=160 .`
--- ukrdc_xml2sqla/models/ukrdc.py
+++ ukrdc_xml2sqla/models/ukrdc.py
@@ -20,6 +20,75 @@ from typing import Optional
 import ukrdc_sqla.ukrdc as orm
 import ukrdc_xsdata.ukrdc.types as xsd_types
 import ukrdc_xsdata.ukrdc as xsd_ukrdc
+import ukrdc_xsdata.ukrdc.lab_orders as xsd_lab_orders
+
+
+class ResultItem:
+    def __init__(self, result_item: xsd_lab_orders.ResultItem):
+        self.result_item = result_item
+
+    def to_orm(self) -> orm.ResultItem:
+        return orm.ResultItem(
+            resulttype=self.result_item.result_type,
+            enteredon=self.result_item.entered_on,
+            prepost=self.result_item.pre_post.value if self.result_item.pre_post else None,
+            serviceidcode=self.result_item.service_id.code if self.result_item.service_id else None,
+            serviceidcodestd=self.result_item.service_id.coding_standard if self.result_item.service_id else None,
+            serviceiddesc=self.result_item.service_id.description if self.result_item.service_id else None,
+            subid=self.result_item.sub_id,
+            resultvalue=self.result_item.result_value,
+            resultvalueunits=self.result_item.result_value_units,
+            referencerange=self.result_item.reference_range,
+            interpretationcodes=self.result_item.interpretation_codes.value if self.result_item.interpretation_codes else None,
+            status=self.result_item.status.value if self.result_item.status else None,
+            observationtime=self.result_item.observation_time,
+            commenttext=self.result_item.comments,
+            referencecomment=self.result_item.reference_comment,
+        )
+
+
+class LabOrder:
+    def __init__(self, laborder: xsd_lab_orders.LabOrder):
+        self.laborder = laborder
+
+    def to_orm(self) -> orm.LabOrder:
+        return orm.LabOrder(
+            receivinglocationcode=self.laborder.receiving_location.code if self.laborder.receiving_location else None,
+            receivinglocationdesc=self.laborder.receiving_location.description if self.laborder.receiving_location else None,
+            receivinglocationcodestd=self.laborder.receiving_location.coding_standard if self.laborder.receiving_location else None,
+            placerid=self.laborder.placer_id,
+            fillerid=self.laborder.filler_id,
+            orderedbycode=self.laborder.ordered_by.code if self.laborder.ordered_by else None,
+            orderedbydesc=self.laborder.ordered_by.description if self.laborder.ordered_by else None,
+            orderedbycodestd=self.laborder.ordered_by.coding_standard if self.laborder.ordered_by else None,
+            orderitemcode=self.laborder.order_item.code if self.laborder.order_item else None,
+            orderitemdesc=self.laborder.order_item.description if self.laborder.order_item else None,
+            orderitemcodestd=self.laborder.order_item.coding_standard if self.laborder.order_item else None,
+            ordercategorycode=self.laborder.order_category.code if self.laborder.order_category else None,
+            ordercategorydesc=self.laborder.order_category.description if self.laborder.order_category else None,
+            ordercategorycodestd=self.laborder.order_category.coding_standard if self.laborder.order_category else None,
+            specimencollectedtime=self.laborder.specimen_collected_time,
+            specimenreceivedtime=self.laborder.specimen_received_time,
+            status=self.laborder.status,
+            prioritycode=self.laborder.priority.code if self.laborder.priority else None,
+            prioritydesc=self.laborder.priority.description if self.laborder.priority else None,
+            prioritycodestd=self.laborder.priority.coding_standard if self.laborder.priority else None,
+            specimensource=self.laborder.specimen_source,
+            duration=self.laborder.duration,
+            patientclasscode=self.laborder.patient_class.code if self.laborder.patient_class else None,
+            patientclassdesc=self.laborder.patient_class.description if self.laborder.patient_class else None,
+            patientclasscodestd=self.laborder.patient_class.coding_standard if self.laborder.patient_class else None,
+            enteredon=self.laborder.entered_on,
+            enteredatcode=self.laborder.entered_at.code if self.laborder.entered_at else None,
+            enteredatdesc=self.laborder.entered_at.description if self.laborder.entered_at else None,
+            enteredatcodestd=self.laborder.entered_at.coding_standard if self.laborder.entered_at else None,
+            enteringorganizationcode=self.laborder.entering_organization.code if self.laborder.entering_organization else None,
+            enteringorganizationdesc=self.laborder.entering_organization.description if self.laborder.entering_organization else None,
+            enteringorganizationcodestd=self.laborder.entering_organization.coding_standard if self.laborder.entering_organization else None,
+            updatedon=self.laborder.updated_on,
+            externalid=self.laborder.external_id,
+            result_items=[ResultItem(item).to_orm() for item in self.laborder.result_items.result_item] if self.laborder.result_items else None,
+        )
 
 
 class FamilyDoctor:
@@ -31,46 +100,20 @@ class FamilyDoctor:
             gpname=self.family_doctor.gpname,
             gpid=self.family_doctor.gpid,
             gppracticeid=self.family_doctor.gppractice_id,
-            addressuse=self.family_doctor.address.use
-            if self.family_doctor.address
-            else None,
-            fromtime=self.family_doctor.address.from_time
-            if self.family_doctor.address
-            else None,
-            totime=self.family_doctor.address.to_time
-            if self.family_doctor.address
-            else None,
-            street=self.family_doctor.address.street
-            if self.family_doctor.address
-            else None,
-            town=self.family_doctor.address.town
-            if self.family_doctor.address
-            else None,
-            county=self.family_doctor.address.county
-            if self.family_doctor.address
-            else None,
-            postcode=self.family_doctor.address.postcode
-            if self.family_doctor.address
-            else None,
-            countrycode=self.family_doctor.address.country.code
-            if self.family_doctor.address and self.family_doctor.address.country
-            else None,
-            countrycodestd=self.family_doctor.address.country.coding_standard
-            if self.family_doctor.address and self.family_doctor.address.country
-            else None,
-            countrydesc=self.family_doctor.address.country.description
-            if self.family_doctor.address and self.family_doctor.address.country
-            else None,
-            contactuse=self.family_doctor.contact_detail.use
-            if self.family_doctor.contact_detail
-            else None,
-            contactvalue=self.family_doctor.contact_detail.value
-            if self.family_doctor.contact_detail
-            else None,
+            addressuse=self.family_doctor.address.use if self.family_doctor.address else None,
+            fromtime=self.family_doctor.address.from_time if self.family_doctor.address else None,
+            totime=self.family_doctor.address.to_time if self.family_doctor.address else None,
+            street=self.family_doctor.address.street if self.family_doctor.address else None,
+            town=self.family_doctor.address.town if self.family_doctor.address else None,
+            county=self.family_doctor.address.county if self.family_doctor.address else None,
+            postcode=self.family_doctor.address.postcode if self.family_doctor.address else None,
+            countrycode=self.family_doctor.address.country.code if self.family_doctor.address and self.family_doctor.address.country else None,
+            countrycodestd=self.family_doctor.address.country.coding_standard if self.family_doctor.address and self.family_doctor.address.country else None,
+            countrydesc=self.family_doctor.address.country.description if self.family_doctor.address and self.family_doctor.address.country else None,
+            contactuse=self.family_doctor.contact_detail.use if self.family_doctor.contact_detail else None,
+            contactvalue=self.family_doctor.contact_detail.value if self.family_doctor.contact_detail else None,
             email=self.family_doctor.email,
-            commenttext=self.family_doctor.contact_detail.comments
-            if self.family_doctor.contact_detail
-            else None,
+            commenttext=self.family_doctor.contact_detail.comments if self.family_doctor.contact_detail else None,
         )
 
 
@@ -88,12 +131,8 @@ class Address:
             county=self.address.county,
             postcode=self.address.postcode,
             countrycode=self.address.country.code if self.address.country else None,
-            countrycodestd=self.address.country.coding_standard
-            if self.address.country
-            else None,
-            countrydesc=self.address.country.description
-            if self.address.country
-            else None,
+            countrycodestd=self.address.country.coding_standard if self.address.country else None,
+            countrydesc=self.address.country.description if self.address.country else None,
         )
 
 
@@ -148,83 +187,33 @@ class Patient:
 
     def to_orm(self):
         return orm.Patient(
-            birthtime=self.xml.birth_time.to_datetime()
-            if self.xml.birth_time
-            else None,
-            deathtime=self.xml.death_time.to_datetime()
-            if self.xml.death_time
-            else None,
+            birthtime=self.xml.birth_time.to_datetime() if self.xml.birth_time else None,
+            deathtime=self.xml.death_time.to_datetime() if self.xml.death_time else None,
             gender=self.xml.gender,
             countryofbirth=self.xml.country_of_birth,
-            ethnicgroupcode=self.xml.ethnic_group.code
-            if self.xml.ethnic_group
-            else None,
-            ethnicgroupcodestd=self.xml.ethnic_group.coding_standard
-            if self.xml.ethnic_group
-            else None,
-            ethnicgroupdesc=self.xml.ethnic_group.description
-            if self.xml.ethnic_group
-            else None,
-            persontocontactname=self.xml.person_to_contact.name
-            if self.xml.person_to_contact
-            else None,
-            persontocontact_contactnumber=self._first_person_to_contact.value
-            if self._first_person_to_contact
-            else None,
-            persontocontact_relationship=self.xml.person_to_contact.relationship
-            if self.xml.person_to_contact
-            else None,
-            persontocontact_contactnumbercomments=self._first_person_to_contact.comments
-            if self._first_person_to_contact
-            else None,
-            persontocontact_contacttype=self._first_person_to_contact.use
-            if self._first_person_to_contact
-            else None,
+            ethnicgroupcode=self.xml.ethnic_group.code if self.xml.ethnic_group else None,
+            ethnicgroupcodestd=self.xml.ethnic_group.coding_standard if self.xml.ethnic_group else None,
+            ethnicgroupdesc=self.xml.ethnic_group.description if self.xml.ethnic_group else None,
+            persontocontactname=self.xml.person_to_contact.name if self.xml.person_to_contact else None,
+            persontocontact_contactnumber=self._first_person_to_contact.value if self._first_person_to_contact else None,
+            persontocontact_relationship=self.xml.person_to_contact.relationship if self.xml.person_to_contact else None,
+            persontocontact_contactnumbercomments=self._first_person_to_contact.comments if self._first_person_to_contact else None,
+            persontocontact_contacttype=self._first_person_to_contact.use if self._first_person_to_contact else None,
             occupationcode=self.xml.occupation.code if self.xml.occupation else None,
-            occupationcodestd=self.xml.occupation.coding_standard
-            if self.xml.occupation
-            else None,
-            occupationdesc=self.xml.occupation.description
-            if self.xml.occupation
-            else None,
-            primarylanguagecode=self.xml.primary_language.code
-            if self.xml.primary_language
-            else None,
-            primarylanguagecodestd=self.xml.primary_language.coding_standard
-            if self.xml.primary_language
-            else None,
-            primarylanguagedesc=self.xml.primary_language.description
-            if self.xml.primary_language
-            else None,
+            occupationcodestd=self.xml.occupation.coding_standard if self.xml.occupation else None,
+            occupationdesc=self.xml.occupation.description if self.xml.occupation else None,
+            primarylanguagecode=self.xml.primary_language.code if self.xml.primary_language else None,
+            primarylanguagecodestd=self.xml.primary_language.coding_standard if self.xml.primary_language else None,
+            primarylanguagedesc=self.xml.primary_language.description if self.xml.primary_language else None,
             death=self.xml.death,
-            updatedon=self.xml.updated_on.to_datetime()
-            if self.xml.updated_on
-            else None,
+            updatedon=self.xml.updated_on.to_datetime() if self.xml.updated_on else None,
             bloodgroup=self.xml.blood_group,
             bloodrhesus=self.xml.blood_rhesus,
-            numbers=[
-                PatientNumber(number).to_orm()
-                for number in self.xml.patient_numbers.patient_number
-            ]
-            if self.xml.patient_numbers
-            else [],
-            names=[Name(name).to_orm() for name in self.xml.names.name]
-            if self.xml.names
-            else [],
-            contact_details=[
-                ContactDetail(contact).to_orm()
-                for contact in self.xml.contact_details.contact_detail
-            ]
-            if self.xml.contact_details
-            else [],
-            addresses=[
-                Address(address).to_orm() for address in self.xml.addresses.address
-            ]
-            if self.xml.addresses
-            else [],
-            familydoctor=FamilyDoctor(self.xml.family_doctor).to_orm()
-            if self.xml.family_doctor
-            else None,
+            numbers=[PatientNumber(number).to_orm() for number in self.xml.patient_numbers.patient_number] if self.xml.patient_numbers else [],
+            names=[Name(name).to_orm() for name in self.xml.names.name] if self.xml.names else [],
+            contact_details=[ContactDetail(contact).to_orm() for contact in self.xml.contact_details.contact_detail] if self.xml.contact_details else [],
+            addresses=[Address(address).to_orm() for address in self.xml.addresses.address] if self.xml.addresses else [],
+            familydoctor=FamilyDoctor(self.xml.family_doctor).to_orm() if self.xml.family_doctor else None,
         )
 
 
@@ -242,9 +231,8 @@ class PatientRecord:
             # updatedate=self.update_date,
             # repositorycreationdate=self.repository_creation_date,
             # repositoryupdatedate=self.repository_update_date,
-            sendingfacility=self.xml.sending_facility.value
-            if self.xml.sending_facility
-            else None,
+            sendingfacility=self.xml.sending_facility.value if self.xml.sending_facility else None,
             sendingextract=self.xml.sending_extract,
             patient=Patient(self.xml.patient).to_orm() if self.xml.patient else None,
+            lab_orders=[LabOrder(order).to_orm() for order in self.xml.lab_orders.lab_order] if self.xml.lab_orders else [],
         )
