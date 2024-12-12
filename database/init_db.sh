a3aa73daa5577e4345703db00e70c6ca3cf8e802
phil <philip.main@renalregistry.nhs.uk>
2024-12-12 16:10:33 +0000

    wrote migration in sql

--- database/init_db.sh
+++ database/init_db.sh
@@ -22,14 +22,25 @@ else
     exit 1
 fi
 
-# initialise alembic
-psql -U postgres -d ukrdc4 <<-EOSQL
-    CREATE TABLE IF NOT EXISTS alembic_version (
-        version_num VARCHAR(32) NOT NULL
-    );
-    INSERT INTO alembic_version (version_num) VALUES ('a1a7539b1a57');
-EOSQL
+# alembic is a pointless waste of life 
 
-psql -U postgres -d ukrdc4 <<-EOSQL
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA extract TO ukrdc;
-EOSQL
+# initialise alembic
+#psql -U postgres -d ukrdc4 <<-EOSQL
+#    CREATE TABLE IF NOT EXISTS alembic_version (
+#        version_num VARCHAR(32) NOT NULL
+#    );
+#    INSERT INTO alembic_version (version_num) VALUES ('a1a7539b1a57');
+#EOSQL
+
+#psql -U postgres -d ukrdc4 <<-EOSQL
+#    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA extract TO ukrdc;
+#EOSQL
+#""
+
+# migrate with sql instead
+if [[ -f scripts/v5_migration.sql ]]; then
+    psql -U postgres -d ukrdc4 -f scripts/v5_migration.sql
+else
+    echo "data_dump.sql not found!"
+    exit 1
+fi
